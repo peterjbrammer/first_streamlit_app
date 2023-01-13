9d704439580095cad23ee72943d900d950e1f77d
pjb <peterbrammer@outlook.com>
2023-01-13 14:12:24 +0800

    header and button text

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,9 +55,11 @@ def get_fruit_load_list():
         return my_cur.fetchall()
 
 # Add a button to load the fruit
-if streamlit.button('Get fruit Load List'):
+streamlit.header("View our Fruit List - Add Youir Favourites")
+if streamlit.button('Get Fruit List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
+    my_cnx.close()
     streamlit.dataframe(my_data_rows)
 
 #  streamlit.stop()
@@ -71,6 +73,7 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a fruit to the List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
+    my_cnx.close()
     streamlit.text(back_from_function)
 streamlit.write('Thanks for adding ', add_my_fruit)
 
